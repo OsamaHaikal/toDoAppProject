917b0c44bb2274ecbb518e694acfa66820b9ff2f
Osama Haikal <ee.osamahaikal@gmail.com>
2021-04-22 18:40:48 +0300

    create to do model

--- to_do_app_project/toDo/models.py
+++ to_do_app_project/toDo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class ToDo(models.Model):
+    userCreater= models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    title = models.CharField(max_length=120)
+    memo = models.TextField(blank=True)
+    created=models.DateTimeField(auto_now_add= True)
+    date_completed= models.DateTimeField(null=True,blank=True) #when someone hits complete it wil fill
+    important = models.BooleanField(default=False)
+    
+    
+    def __str__(self):
+        return self.title
